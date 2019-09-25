cb7ee7029252a26cad4d988829a0f05922c560db
Nils Balduin <nils.balduin@jtl-software.com>
2019-09-25 09:09:30 +0200

    First Lua blyat

---
+++ NameFlex.lua
@@ -0,0 +1,29 @@
+--Visit us at Legendentreff.de - CS:GO Cheating Community
+--Get updates frequently on Discord: https://discordapp.com/invite/enpEU3W
+
+local Misc_Ref = gui.Reference("MISC", "GENERAL", "Main");
+local isPressed = false;
+local spammed_last  = globals.TickCount();
+local spamName = client.GetConVar("name");
+
+local function buttonPressed()
+	isPressed = true;
+end
+local Apply_Button = gui.Button( Misc_Ref, "Nameflex", buttonPressed )
+
+function nameSpam()
+	local localPlayer = entities.GetLocalocalPlayerlayer()
+	if (localPlayer == nil) then
+		isPressed = false;
+	end
+	if (isPressed) then
+		if globals.TickCount() - spammed_last > 10 then
+			client.SetConVar("name", spamName .. ' ', false);
+			spammed_last = globals.TickCount();
+		end
+	end
+end
+callbacks.Register( "Draw", "name Spam", nameSpam);
+
+--Lua made by nn 
+--For more Luas: https://github.com/nn0001/legendentreff
